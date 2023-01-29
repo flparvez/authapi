e0883284c94e6e14cd4a7b311b1581d7a554c07a
Md Parvez <98581345+flparvez@users.noreply.github.com>
2023-01-29 22:56:13 +0600

    updates

--- authapi/settings.py
+++ authapi/settings.py
@@ -174,4 +174,5 @@ PASSWORD_RESET_TIMEOUT=900          # 900 Sec = 15 Min
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
+    "https://flparvez-blog.netlify.app/",
 ]
